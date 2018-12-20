8854fb7877671fb83e12d833b8a0891a17c7b498
Andreas Schultz <andreasschultz@Andreass-MBP.fritz.box>
2018-12-20 14:41:02 +0100

    Collection View IB Setup

---
+++ Coder Swag/Coder Swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  Coder Swag
+//
+//  Created by Andreas Schultz on 20.12.18.
+//  Copyright © 2018 Andreas Schultz. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
